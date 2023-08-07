bc0129f01a6072f725ac8f9a51668ea466bb4f9f
Gabriela Aguilera <gabrielaaguilera@Gabrielas-MacBook-Pro.local>
2023-08-07 13:49:28 +0200

    archivo index.html y desarrollo del primer ejercicio

---
+++ Desktop/Proyectos/practicaJS/ejercicio1.js
@@ -0,0 +1,12 @@
+const miObjeto = {
+    nombre: 'Gabriela',
+    apellido: 'Aguilera',
+    temasBootcamp: ['GitYGitHub', 'HTML', 'CSS', 'JavaScript'],
+    estaEnBusquedaActiva: true,
+    linkRedes: [
+        {
+            nombreRedSocial: 'GitHub', link: 'https://github.com/GrabyAprende'
+        }
+    ],
+};
+console.log(miObjeto);
